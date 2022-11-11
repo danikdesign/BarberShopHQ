be7930dfbe54f107a8f5b86dda78394cf7dd2772
danikdesign <danik.newmail@gmail.com>
2022-11-11 15:28:16 +0200

    Add Clients & Barbers migrates

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db"
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
+end
 
+class Barber < ActiveRecord::Base
 end
 
 get '/' do
