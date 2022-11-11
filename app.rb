6038a884eebb66db6eef09090eb73179ad9412da
danikdesign <danik.newmail@gmail.com>
2022-11-11 16:55:57 +0200

    Add views /visit, /contacts, /message. Add contacts migration. Fix some html & js bugs

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 #encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
+
+before do
+  @barbers = Barber.order 'created_at DESC'
+end
 
 class Client < ActiveRecord::Base
 end
@@ -11,7 +16,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Contact < ActiveRecord::Base
+end
+
 get '/' do
-  @barbers = Barber.all
   erb :index
+end
+
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datestamp = params[:datestamp]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  Client.create(name: @username, phone: @phone, datestamp: @datestamp, barber: @barber, color: @color)
+
+  @title = 'Вы успешно записались. До встречи!'
+  @message = "Вы записаны к нам в BarberShop #{@datestamp} к мастеру: #{@barber}. Вы выбрали цвет окрашивания #{@color}"
+  erb :message
 end
